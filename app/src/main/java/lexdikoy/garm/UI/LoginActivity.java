b212661d8587bf0cecfbe02b463bc97cbd5c9487
Alexandr Kuznecov <kalaver@yandex.ru>
2019-04-09 19:39:56 +0300

    Initial commit

---
+++ app/src/main/java/lexdikoy/garm/UI/LoginActivity.java
@@ -0,0 +1,26 @@
+package lexdikoy.garm.UI;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Objects;
+
+import lexdikoy.garm.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+}
